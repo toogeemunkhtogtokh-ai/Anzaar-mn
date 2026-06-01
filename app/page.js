0e062f9a7f5a671ca9c3a3de0d132cfdd8be2caa
toogeemunkhtogtokh-ai <toogeemunkhtogtokh@gmail.com>
2026-06-01 15:04:03 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      background: "#000",
+      color: "#fff",
+      minHeight: "100vh",
+      padding: "60px",
+      fontFamily: "serif"
+    }}>
+      <h1 style={{color:"#ff0000"}}>Anzaar.mn</h1>
+      <p>Өнөөдрийг анзаарч маргаашийг бүтээе</p>
+    </main>
+  )
+}
